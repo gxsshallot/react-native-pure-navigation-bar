f5c256677872bdc8d161fe3bcdd7982c774a894b
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-02-06 00:46:42 +0800

    Fix: test file location error.

--- __tests__/example.js
+++ __tests__/example.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import {Example} from '../example';
+import {Example} from '../example/example';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
